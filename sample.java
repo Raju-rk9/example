fa347f4a1d3895c41c7e2cd1149443d3c75b539c
Raju-rk9 <rajurk.23ece@kongu.edu>
2026-04-27 16:10:09 +0530

    initial commit

---
+++ sample.java
@@ -0,0 +1,9 @@
+class sample{
+     public static void main(String ar[]) 
+     {
+        int a = 10; 
+        char b = 'm';
+        System.out.println(a);
+        System.out.println(b); 
+        }
+        }                
---
+++ strs.java
@@ -0,0 +1,22 @@
+class strs{
+    public static void main(String[] args){
+        // String s1 = "raju";
+        // String s2 = "raju";
+        // System.out.print(s1==s2);
+        int a= 3;
+        int b= 2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a>>b);
+        System.out.println(a<<b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a++);
+        System.out.println(++a);
+        System.out.println(a>b);
+        System.out.println(a<b);
+      
+    }
+}
