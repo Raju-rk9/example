4992eea2bea2d2b27a968ffbd6712e9e1d8b5bdf
Raju-rk9 <rajurk.23ecce@kongu.edu>
2026-04-30 13:47:14 +0530

    day 2 of java programming

---
+++ day3/loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class loop{
+    public static void main(String[] args) {
+        Scanner ra = new Scanner(System.in);
+        int n=1;
+        do { 
+           n = ra.nextInt();
+          also suggest me is it  better to study neet durng our higher studing
+           System.out.println("User Input: "+n); 
+        } while (n!=0);
+        
+    }
+}
---
+++ day3/vote.java
@@ -0,0 +1,13 @@
+class vote{
+    public static void main(String args[]){
+        int a =18;
+        if (a>=18){
+            System.out.println("you are eligiable for vote");
+        }
+        else 
+        {
+            System.out.println("you are not eligiable for vote");  
+        }
+        
+    }
+}
