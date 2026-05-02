4911b0057b19e474fa5a52480519079103385c83
Raju-rk9 <rajurk.23ecce@kongu.edu>
2026-05-02 12:14:49 +0530

    day 2 of java foundation

---
+++ day2/str.java
@@ -0,0 +1,8 @@
+class str{
+    public static void main(String[] args){
+         String s = "raju";
+         String s1 = new String("abc");
+         System.out.println(s == s1);
+         System.out.println(s.equals(s1));
+    }
+}
